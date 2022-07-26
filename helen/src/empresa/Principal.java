f57df8cb14a912f726031fe1feda734d4532ff4b
John Castillo <john.gnu@gmail.com>
2022-07-26 13:31:13 -0400

    a 2

--- helen/src/empresa/Principal.java
+++ helen/src/empresa/Principal.java
@@ -11,7 +11,7 @@ public class Principal {
         Producto p1 = new Producto("prod1", 20, 5, 2023, "SI");
         Producto p2 = new Producto("prod2", 20, 5, 2023, "NO");
         Producto p3 = new Producto("prod3", 20, 5, 2024, "NO");
-        Producto p4 = new Producto("prod4", 20, 5, 2025, "SI");
+        Producto p4 = new Producto("prod4", 20, 5, 2024, "SI");
 
         colaProducto cl = new colaProducto(5);
         cl.adiCola(p1);
@@ -19,6 +19,10 @@ public class Principal {
         cl.adiCola(p3);
         cl.adiCola(p4);
 
+        cl.mostrarColaSimple();
+        
+        // a)
+        llevarAlPrincipio(cl, 2024);
         cl.mostrarColaSimple();
         
         // b)
@@ -38,6 +42,28 @@ public class Principal {
             caux.adiCola(x);
         }
         cl.vaciar(caux);
+        System.out.println();
+    }
+    
+    public static void llevarAlPrincipio(colaProducto cl, int a) {
+        colaProducto caux = new colaProducto(cl.max);
+        colaProducto caux2 = new colaProducto(cl.max);
+        Producto x;
+        while (!cl.esVacia()) {
+            x = cl.eliCola();
+            if (x.getAnioVenc() == a) {
+                caux2.adiCola(x);
+            } else {
+                caux.adiCola(x);
+            }
+        }
+        
+        while (!caux2.esVacia()) {
+            x = caux2.eliCola();
+            cl.adiCola(x);
+        }
+        cl.vaciar(caux);
+        
         System.out.println();
     }
 }
