1a6e2a168cd3a8db3c65ae6a8457d47f93141b64
John Castillo <john.gnu@gmail.com>
2022-07-26 12:43:44 -0400

    pila

--- helen/src/helen/Principal.java
+++ helen/src/helen/Principal.java
@@ -10,7 +10,20 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola helen");
+        Plato p1 = new Plato("fricase", 20);
+        Plato p2 = new Plato("silpancho", 10);
+        Plato p3 = new Plato("majadito", 10);
+        Plato p4 = new Plato("asado", 10);
+        Plato p5 = new Plato("milanesa", 12);
+        
+        pilaPlato pi = new pilaPlato(5);
+        pi.adiPila(p1);
+        pi.adiPila(p2);
+        pi.adiPila(p3);
+        pi.adiPila(p4);
+        pi.adiPila(p5);
+        
+        pi.mostrarPila();
     }
     
 }
--- helen/src/helen/pilaPlato.java
+++ helen/src/helen/pilaPlato.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package helen;
 
 /**
@@ -10,5 +5,68 @@ package helen;
  * @author desktop
  */
 public class pilaPlato {
-    
+
+    private Plato p[];
+    int max, tope;
+
+    public pilaPlato(int m) {
+        max = m;
+        p = new Plato[max];
+        tope = -1;
+    }
+
+    public boolean esVacia() {
+        if (tope == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean esLlena() {
+        if (tope == (max - 1)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void adiPila(Plato x) {
+        if (esLlena()) {
+            System.out.println("Pila llena");
+        } else {
+            tope = tope + 1;
+            p[tope] = x;
+        }
+    }
+
+    public Plato eliPila() {
+        Plato x = null;
+        if (esVacia()) {
+            System.out.println("Pila vacia");
+        } else {
+            x = p[tope];
+            tope = tope - 1;
+        }
+        return x;
+    }
+
+    public void mostrarPila() {
+        pilaPlato paux = new pilaPlato(max);
+        Plato x;
+        while (!esVacia()) {
+            x = eliPila();
+            x.mostrar();
+            paux.adiPila(x);
+        }
+        vaciarPila(paux);
+        System.out.println();
+    }
+
+    public void vaciarPila(pilaPlato paux) {
+        while (!paux.esVacia()) {
+            Plato x = paux.eliPila();
+            adiPila(x);
+        }
+    }
 }
