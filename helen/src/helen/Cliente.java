09347985c22f1d9aafc9714c1218601b18faab16
John Castillo <john.gnu@gmail.com>
2022-07-26 12:39:08 -0400

    ini

---
+++ helen/src/helen/Cliente.java
@@ -0,0 +1,40 @@
+package helen;
+
+/**
+ *
+ * @author desktop
+ */
+public class Cliente {
+
+    private String nombre;
+    private String nit;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, String nit) {
+        this.nombre = nombre;
+        this.nit = nit;
+    }
+
+    public void mostrar() {
+        System.out.println("nombre: " + nombre + " nit: " + nit);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+}
