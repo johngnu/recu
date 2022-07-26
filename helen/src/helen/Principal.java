ef2686d3391082e35cc0cfe6d0bd05172a1a3df0
John Castillo <john.gnu@gmail.com>
2022-07-26 07:54:48 -0400

    start

---
+++ helen/src/helen/Principal.java
@@ -0,0 +1,16 @@
+package helen;
+
+/**
+ *
+ * @author desktop
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola helen");
+    }
+    
+}
